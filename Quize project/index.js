935c8880746a018ee4c93d6d56dbfea165ba00a1
Suraj Kr <surajkr739@gmail.com>
2022-01-13 20:44:05 +0530

    Add files via upload

---
+++ Quize project/index.js
@@ -0,0 +1,38 @@
+gsap.from(".logo", {
+  opacity: 0,
+  duration: 1.0,
+  delay: 1,
+  x: -100,
+});
+gsap.from("ul", {
+  opacity: 0,
+  duration: 1.1,
+  delay: 1,
+  x: 100,
+});
+gsap.from("h2",{
+    opacity:0,
+    duration:1,
+    delay:1.2,
+    y:-50,
+})
+gsap.from("p", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.3,
+  y: 50,
+});
+gsap.from("h4", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.9,
+  y: -50,
+});
+
+gsap.from("#btn", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.99999,
+  y: 50,
+});
+
